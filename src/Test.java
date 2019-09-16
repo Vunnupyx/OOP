78cc07892f2a3ae611ed1560a66f564d6d81e6d9
Vunnupyx <muhutdinovrifat@yandex.ru>
2019-09-16 22:27:27 +0400

    clear

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,4 @@
 public class Test {
     public static void main(String[] args) {
-        ExtendShow ext = new ExtendShow();
-        SuperShow sup = new SuperShow();
-        ext.show();
-        sup.show();
-        System.out.println("ext.str = " + ext.str);
-        System.out.println("sup.str = " + sup.str);
-
-    }
-}
-
-class ExtendShow extends SuperShow {
-    public String str = "ExtendStr";
-    public void show() {
-        System.out.println("Extend.show: " + str);
     }
 }
